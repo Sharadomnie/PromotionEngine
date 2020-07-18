cf2cfbdfe1f4c54440716a5ba873c01a8275f8cd
sharadomnie <sharadsaini1989@gmail.com>
2020-07-18 16:44:05 +0530

    WebApi Ajax call

---
+++ PromotionEngine/Scripts/Index.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    $("#buttonCalculateCost").click(function () {
+        var sku = new Object();
+        sku.SkuIds = $.trim($('#SkuIds').val());
+        sku.PromotionName = $.trim($('#PromotionTypeName').val());
+        $.ajax({
+            url: 'http://localhost:1819/api/values/',
+            type: 'POST',
+            dataType: 'json',
+            data: sku,
+            success: function (data) {
+                console.log(data);
+                $("#labelCost").text('');
+                $("#labelCost").text(data);
+            },
+            error: function (e) {
+                console.log('Error in Operation'+e);
+            }
+        });
+    });
+});
