524e176065fe732ba1ebec64628fb5fbf4d6e995
sharadomnie <sharadsaini1989@gmail.com>
2020-07-18 18:35:56 +0530

    Applying CD promotionType

--- BusinessLayer/Calculation.cs
+++ BusinessLayer/Calculation.cs
@@ -44,7 +44,41 @@ namespace BusinessLayer
           }
 
 
-            
+             if (sku.PromotionName=="CD")
+            {
+               
+
+                var totalCountofSku1 = selectedSkuIdCount[sku.PromotionName[0].ToString()];                 
+                var totalCountofSku2 = selectedSkuIdCount[sku.PromotionName[1].ToString()];
+                if (totalCountofSku1 + totalCountofSku2 > 1)
+                {
+                        var costOfSku1 = skuIdCostMapping[sku.PromotionName[0].ToString()];
+                        var costOfSku2 = skuIdCostMapping[sku.PromotionName[1].ToString()];
+
+                        var isSku1PriceGreaterThanSku2 = costOfSku1 > costOfSku2 ? true : false;
+
+                        var difference = isSku1PriceGreaterThanSku2 ? totalCountofSku1 - totalCountofSku2 : totalCountofSku2 - totalCountofSku1;
+                        if (difference == 0)
+                        {
+                            actualCost = actualCost - (totalCountofSku1 * skuIdCostMapping[sku.PromotionName[0].ToString()]);
+                            actualCost = actualCost - (totalCountofSku2 * skuIdCostMapping[sku.PromotionName[1].ToString()]);
+
+                            costAfterApplyingPromotion = appliedPromotionCost * totalCountofSku1;
+                            actualCost = actualCost + costAfterApplyingPromotion;
+
+                        }
+                        else if (difference > 0)
+                        {
+
+                        }
+                        else if (difference < 0)
+                        {
+
+                        }
+
+             }
+            }
+            else{
          
               var totalCountofthisSku = selectedSkuIdCount[sku.PromotionName[0].ToString()];              
               var numberOfTimes = sku.PromotionName.Length;
@@ -62,7 +96,7 @@ namespace BusinessLayer
                   actualCost = actualCost + costAfterApplyingPromotion;
               }
 
-
+            }
 
 
 
